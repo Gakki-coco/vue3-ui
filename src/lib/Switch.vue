416c491b9eb31769094a31da13d1f6699aaa17fe
Dolores <alyciahathaways@gmail.com>
2020-09-02 21:44:19 +0800

    细节优化

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -26,31 +26,44 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
-
-button.checked {
-  background: blue;
 
   > span {
-    left: calc(100% - #{$h2} - 2px);
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
   }
-}
 
-button:focus {
-  outline: none;
+  &.checked {
+    background: #1890ff;
+
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
 }
 </style>
