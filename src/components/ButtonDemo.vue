70c012b5e4e1218747e6ef9738dc0b60e7926539
Dolores <alyciahathaways@gmail.com>
2020-09-06 18:03:44 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>示例一</h2>
-  <div>
+  <div class="section">
     <Button>你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
@@ -8,9 +8,39 @@
   </div>
   <h2>示例二</h2>
   <div>
-    <Button>普通</Button>
-    <Button size="big">大大大</Button>
-    <Button size="small">小小小</Button>
+    <div class="section">
+      <Button>普通</Button>
+      <Button size="big">大大大</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div class="section">
+      <Button theme="link">普通</Button>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link" size="small">小小小</Button>
+    </div>
+    <div class="section">
+      <Button theme="text">普通</Button>
+      <Button theme="text" size="big">大大大</Button>
+      <Button theme="text" size="small">小小小</Button>
+    </div>
+  </div>
+  <h2>示例三</h2>
+  <div>
+    <div class="section">
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div class="section">
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div class="section">
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </div>
   </div>
 </template>
 
@@ -24,3 +54,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.section {
+  margin-bottom: 8px;
+}
+</style>
