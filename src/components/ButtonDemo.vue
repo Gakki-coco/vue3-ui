cd723ae9d74777fe02587c148cf81f52ed8442aa
Dolores <alyciahathaways@gmail.com>
2020-09-06 17:46:28 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,17 @@
 <template>
-  <Button>你好</Button>
-  <Button theme="button">你好</Button>
-  <Button theme="link">你好</Button>
-  <Button theme="text">你好</Button>
+  <h2>示例一</h2>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h2>示例二</h2>
+  <div>
+    <Button>普通</Button>
+    <Button size="big">大大大</Button>
+    <Button size="small">小小小</Button>
+  </div>
 </template>
 
 <script lang="ts">
